58ee01822d2261e17c64836e93423098a5321964
Swikriti <swikriti808@gmail.com>
2020-04-17 14:10:40 +0545

    rect

---
+++ Rectangle.java
@@ -0,0 +1,10 @@
+public class Rectangle implements Shape{
+	public void draw(){
+		System.out.println("Inside Rectangle");
+	}
+	 public static void main(String[] args) {
+	 	Rectangle c1= new Rectangle();
+	 	c1.draw();
+		
+	}
+} 
