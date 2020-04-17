adf1d671ee94771a4d6c246f53d9efe1756690da
Swikriti <swikriti808@gmail.com>
2020-04-17 14:11:23 +0545

    square

---
+++ Square.java
@@ -0,0 +1,10 @@
+public class Square implements Shape{
+	public void draw(){
+		System.out.println("Inside square");
+	}
+	 public static void main(String[] args) {
+	 	Square c1= new Square();
+	 	c1.draw();
+		
+	}
+} 
